99460754a333719f619700bb6f6651ce04a12c4c
castillo05 <jorgeantonio20142014@gmail.com>
2020-08-10 15:52:23 -0600

    [Add] modelo de students

---
+++ dist/models/students.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const students = sequelize.define('students', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: DataTypes.STRING,
+    schedule: DataTypes.STRING,
+    dateStart: DataTypes.STRING,
+    dateEnd: DataTypes.STRING,
+    numberStudents: DataTypes.INTEGER
+  }, {});
+  return students;
+};
